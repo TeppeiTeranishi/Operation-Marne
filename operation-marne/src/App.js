d9f66b1b60835757d345dce41da955e55546d6ea
Ademir Candelario R <ademircandelario07@hotmail.com>
2021-08-31 19:57:40 -0600

    adding a little section called info

--- operation-marne/src/App.js
+++ operation-marne/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import Cover from './components/cover/Cover';
 import Navbar from './components/navbar/Navbar';
 import About from './components/about/About';
-import Slider from './components/slider/Slider'
+import Slider from './components/slider/Slider';
+import Info from './components/info/Info';
 
 function App() {
   const [scrollHeight, setScrollHeight] = useState(0);
@@ -23,6 +24,7 @@ function App() {
       <Cover />
       <About />
       <Slider />
+      <Info />
     </div>
   );
 }
